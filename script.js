5a4c876f711e4948e43be57d4981dfae129b92fd
Juan P. Romano <programming@jpromano.net>
2023-02-25 23:23:01 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+ (15 sloc)  425 Bytes
+const textEl = document.getElementById('text')
+const speedEl = document.getElementById('speed')
+const text = 'We Love Programming!'
+let idx = 1
+let speed = 300 / speedEl.value
+
+writeText()
+
+function writeText() {
+    textEl.innerText = text.slice(0, idx)
+
+    idx++
+
+    if(idx > text.length) {
+        idx = 1
+    }
+
+    setTimeout(writeText, speed)
+}
+
+
+speedEl.addEventListener('input', (e) => speed = 300 / e.target.value)
+Footer
